29cf1ba29e9a5236ddf37d19d02f7f125c608b90
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-05-12 14:56:03 +0200

    okay

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict';
-$(window).scroll(function () {
-if ($(window).scrollTop() >= 47) {
-$('div#navigation table tr td a').css('opacity','0.7');
-} else {
-$('div#navigation table tr td a').css('opacity','1');
-}
+$(window).scroll(function() {
+  if ($(window).scrollTop() >= 47) {
+    $('div#navigation table tr td a').css('opacity', '0.7');
+  } else {
+    $('div#navigation table tr td a').css('opacity', '1');
+  }
 });
