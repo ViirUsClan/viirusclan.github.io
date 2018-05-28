6e65e1d4b90472c0f1a77a0360a80a7b1f2e1166
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-05-29 01:14:11 +0200

    ordered everything and named it

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
 $(document).ready(function() {
   $(".menu-icon").on("click", function() {
     $("nav ul").toggleClass("zeigebitte");
+    $("div.menu-icon").toggleClass("gahtdas");
   });
 });
 // Scrolling Effect
 $(window).on("scroll", function() {
-  if ($(window).scrollTop()) {
+  if ($(window).scrollTop() >= 222) {
     $('nav').addClass('black');
   } else {
     $('nav').removeClass('black');
