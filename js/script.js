a76d05ee56baa485db55b3dfdae135cc84c537b6
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-06-01 09:20:29 +0200

    some typos // another way to js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,24 @@ $(document).ready(function() {
     $("div.menu-icon").toggleClass("gahtdas");
   });
 });
-// Scrolling Effect
+// Menuband wird schwarz beim Scrollen
+/*
 $(window).on("scroll", function() {
   if ($(window).scrollTop() >= 222) {
     $('nav').addClass('black');
   } else {
     $('nav').removeClass('black');
+  }
+})
+*/
+$(window).on("scroll", function() {
+  if ($(window).scrollTop() >= 222) {
+    $('nav').css({
+      'background: rgba(0, 0, 0, 0.8)',
+    });
+  } else {
+    $('nav').css({
+      'background: rgba(0, 0, 0, 0)';
+    });
   }
 })
