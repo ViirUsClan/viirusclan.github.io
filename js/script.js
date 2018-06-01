0e395a6d711ca4ab06b63cd707e90ee1b3b5fce8
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-06-01 09:36:41 +0200

    redo this shit

--- js/script.js
+++ js/script.js
@@ -6,23 +6,10 @@ $(document).ready(function() {
   });
 });
 // Menuband wird schwarz beim Scrollen
-/*
 $(window).on("scroll", function() {
   if ($(window).scrollTop() >= 222) {
     $('nav').addClass('black');
   } else {
     $('nav').removeClass('black');
-  }
-})
-*/
-$(window).on("scroll", function() {
-  if ($(window).scrollTop() >= 222) {
-    $('nav').css({
-      'background: rgba(0, 0, 0, 0.8)',
-    });
-  } else {
-    $('nav').css({
-      'background: rgba(0, 0, 0, 0)';
-    });
   }
 })
